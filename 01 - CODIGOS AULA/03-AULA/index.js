5530fd79e3bf1a6981772cc31399f8f24d9bad94
jameshmaclean <james.hector.maclean@gmail.com>
2023-05-02 18:11:39 -0300

    Envio de codigos da aula e exercicios

---
+++ 01 - CODIGOS AULA/03-AULA/index.js
@@ -0,0 +1,41 @@
+const pessoa = {
+  nome: "James",
+  idade: 28,
+};
+
+// if (pessoa.idade <= 0) {
+//   console.log("Você ainda não nasceu");
+// } else if (pessoa.idade >= 18) {
+//   console.log(`${pessoa.nome} é maior de idade`);
+// } else {
+//   console.log("Menor de idade");
+// }
+// pessoa.idade > 18
+//   ? console.log("Maior de Idade")
+//   : console.log("Menor de idade");
+
+// for(let i = 0 ; i<10;i++){
+//     console.log(i+1)
+// }
+
+// let i = 0;
+// while( i<10){
+//     console.log(i)
+//     i++
+// }
+// let count=0;
+// do{
+//     console.log(count)
+//     count++
+// } while (count <4)
+
+function imprimeOi(){
+    console.log("Oi")
+}
+
+function imprime(frase){
+    console.log(frase)
+}
+
+imprimeOi();
+imprime("Alooooooo")
